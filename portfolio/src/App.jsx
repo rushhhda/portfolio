6587051675d85796e38fd485abad5317a2d68069
rushhda <rushdasajid0210@gmail.com>
2026-03-16 21:18:45 +0530

    project added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -30,6 +30,13 @@ function App() {
       gitHub: "https://github.com/rushhhda/Pig-game",
       live: "https://piggggamee.netlify.app/",
     },
+    {
+      img: "https://img.freepik.com/premium-vector/photo-gallery-illustration-museum-visitor-view-exhibition-modern-abstract-paintings-picture_2175-23903.jpg?semt=ais_hybrid&w=740&q=80",
+      pName: "PHOTO GALLERY",
+      desc: "React and tailwind based project-Photo gallery with search and favourites feature.",
+      gitHub: "https://github.com/rushhhda/photoGallery",
+      live: "https://photogallryy.netlify.app/",
+    },
   ];
   return (
     <div className="parent">
